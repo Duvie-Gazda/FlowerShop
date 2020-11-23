ad80d6d8c2276ef7fb390760384f99ca035db939
Duvi-Gazda <gazda.haimdavid@gmail.com>
2020-11-23 11:10:19 +0200

    add simple framework

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '35b618a047f4477a21b1089a1d3971459ed24b82',
+    'name' => 'david/php-mvc-framework',
+  ),
+  'versions' => 
+  array (
+    'david/php-mvc-framework' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '35b618a047f4477a21b1089a1d3971459ed24b82',
+    ),
+  ),
+);
